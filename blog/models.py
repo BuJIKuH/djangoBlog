517d95a4665546bac8d8838f0f304b86dceaf3ee
BuJIKuH <klassniy.jew@gmail.com>
2022-10-19 14:28:25 +0300

    добавлено название в таблицу

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField('Название', max_length=200)
     text = models.TextField("Содержание")
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField("Дата создания", default=timezone.now)
+    published_date = models.DateTimeField("Дата публикации",blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
